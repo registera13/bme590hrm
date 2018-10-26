25fdc8717d86c413107b2ba8d6792b27bd52781b
azr3_desktop <registera13@gmail.com>
2018-10-26 16:27:58 -0400

    finish test_readdata

--- test_ReadData.py
+++ test_ReadData.py
@@ -1,7 +1,18 @@
 import pytest
+import numpy
 
-def test_Inputs():
+def test_ReadData():
+    """
+    Test input datafile 4 first and last time and voltage is correct
+    :return:
+    """
     from ReadData import DataIO
-    testObj1 = DataIO(filename='test_data1.csv')
+    testObj1 = DataIO('test_data4.csv')
+    testObj1.read_data()
+    assert testObj1.time[0] == 0
+    assert testObj1.voltage[0] == -0.375
+    assert testObj1.time[-1] == 27.775
+    assert testObj1.voltage[-1] == -0.365
+
 
 
